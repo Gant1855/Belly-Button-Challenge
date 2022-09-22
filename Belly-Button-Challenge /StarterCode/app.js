7ee9209e3420308b798f26388f264ce38fd2d2a8
Gant1855 <106410555+Gant1855@users.noreply.github.com>
2022-09-22 19:08:53 -0400

    Add files via upload

---
+++ Belly-Button-Challenge /StarterCode/app.js
@@ -0,0 +1,143 @@
+function getPlot(id) {
+   
+    d3.json("Data/samples.json").then((data)=> {
+        console.log(data)
+  
+        var wfreq = data.metadata.map(d => d.wfreq)
+        console.log(`Washing Freq: ${wfreq}`)
+        
+  
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        console.log(samples);
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
+        var OTU_top = (samples.otu_ids.slice(0, 10)).reverse();
+        var OTU_id = OTU_top.map(d => "OTU " + d)
+        var labels = samples.otu_labels.slice(0, 10);
+        var trace = {
+            x: samplevalues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+              color: 'rgb(142,124,195)'},
+            type:"bar",
+            orientation: "h",
+        };
+
+        var data = [trace];
+  
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };
+        // bar chart 
+        Plotly.newPlot("bar", data, layout);
+      
+        //bubble chart
+        var trace1 = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+  
+        };
+  
+        //for the bubble plot
+        var layout_b = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+  
+        // creating data variable 
+        var data1 = [trace1];
+  
+        // create the bubble plot
+        Plotly.newPlot("bubble", data1, layout_b); 
+  
+        // guage chart
+  
+        var data_g = [
+          {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: parseFloat(wfreq),
+          title: { text: `Weekly Washing Frequency ` },
+          type: "indicator",
+          
+          mode: "gauge+number",
+          gauge: { axis: { range: [null, 9] },
+                   steps: [
+                    { range: [0, 2], color: "yellow" },
+                    { range: [2, 4], color: "cyan" },
+                    { range: [4, 6], color: "teal" },
+                    { range: [6, 8], color: "lime" },
+                    { range: [8, 9], color: "green" },
+                  ]}
+              
+          }
+        ];
+        var layout_g = { 
+            width: 700, 
+            height: 600, 
+            margin: { t: 20, b: 40, l:100, r:100 } 
+          };
+        Plotly.newPlot("gauge", data_g, layout_g);
+      });
+  }  
+//function to get the necessary data
+function getInfo(id) {
+    // read the json file to get data
+    d3.json("Data/samples.json").then((data)=> {
+        
+        // get the metadata info for the demographic panel
+        var metadata = data.metadata;
+
+        console.log(metadata)
+
+        // filter by id 
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+
+        // select demographic panel
+        var demographicInfo = d3.select("#sample-metadata");
+        
+        // empty the info panel each time before getting new id info
+        demographicInfo.html("");
+
+        // grab the  data for the id and append info to panel
+        Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+        });
+    });
+
+// create the function for the rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("Data/samples.json").then((data)=> {
+        console.log(data)
+
+        // the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+        // display the data and the plots to the page
+        getPlot(data.names[0]);
+        getInfo(data.names[0]);
+    });
+}
+
+init();
